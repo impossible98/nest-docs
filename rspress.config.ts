e56cf03377f4006e8613953627556397539ecf7b
impossible98 <christoleavenow@outlook.com>
2024-08-06 13:11:17 +0000

    feat: add first-steps.md

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,10 +1,12 @@
+// import built-in modules
 import * as path from 'path';
+// import third-party modules
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'NestJS',
-  description: 'Rspack-based Static Site Generator',
+  description: 'About Nest',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
@@ -12,7 +14,29 @@ export default defineConfig({
   },
   themeConfig: {
     socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+      {
+        icon: 'github',
+        mode: 'link',
+        content: 'https://github.com/web-infra-dev/rspress',
+      },
     ],
+    sidebar: {
+      '/': [
+        {
+          text: 'INTRODUCTION',
+          link: '/introduction',
+        },
+        {
+          text: 'OVERVIEW',
+          collapsible: false,
+          items: [
+            {
+              text: 'First steps',
+              link: '/overview/first-steps',
+            },
+          ],
+        },
+      ],
+    },
   },
 });
