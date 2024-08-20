a54b8f5d190bd59d4e8b5c2abd27dd6ea13e5878
impossible98 <christoleavenow@outlook.com>
2024-08-20 21:16:36 +0800

    feat: add nats.md

--- rspress.config.ts
+++ rspress.config.ts
@@ -299,6 +299,10 @@ export default defineConfig({
               text: 'MQTT',
               link: '/microservices/mqtt',
             },
+            {
+              text: 'NATS',
+              link: '/microservices/nats',
+            },
           ],
         },
         {
